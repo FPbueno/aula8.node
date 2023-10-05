62a36e3f378c8643b79660e2bc9c6f9a3c2e606c
Fillipe Almeida <120722022+FPbueno@users.noreply.github.com>
2023-10-04 21:45:20 -0300

    Add files via upload
    exercicios aula 8 node

---
+++ src/exercicio5.js
@@ -0,0 +1,21 @@
+disciplina = {
+  nome: "Algoritmos",
+  carga: 80,
+  notas: [
+    {
+      peso: 0.25,
+      nota: 8.2,
+    },
+    {
+      peso: 0.35,
+      nota: 7.5,
+    },
+    {
+      peso: 0.4,
+      nota: 9,
+    },
+  ],
+};
+
+r = (disciplina.notas[0].peso * disciplina.notas[0].nota) + (disciplina.notas[1].peso * disciplina.notas[1].nota) + (disciplina.notas[2].peso * disciplina.notas[2].nota);
+console.log("Nota final:", r); 
